f29c97b0352cd6ac58307e7f88e51196c2b405ec
ValdemarAndelius <118256888+ValdemarAndelius@users.noreply.github.com>
2024-03-25 14:48:39 +0100

    h

--- app.rb
+++ app.rb
@@ -11,8 +11,28 @@ get('/')  do
     slim(:start)
 end 
 
-get ('/login') do
+get('/login') do
     @username = session[:username]
     @password = session[:password]
     slim(:login)
+end
+
+get('/build') do
+    db = SQLite3::Database.new("db/slutprojekt2024.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM part")
+    slim(:"build/index",locals:{build:result})
+end
+
+get('/build/new') do
+    slim(:"build/new")
+end
+
+post('/build/new') do
+    title = params[:part]
+    artist_id = params[:part_id].to_i
+    p "Vi fick in datan #{part} #{part_id}"
+    db = SQLite3::Database.new("db/slutprojekt2024.db")
+    db.execute("INSERT INTO build (part, part_id) VALUES (?,?)", part, part_id)
+    redirect('/build')
 end
